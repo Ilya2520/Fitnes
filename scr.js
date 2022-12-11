86eff49f52b56533dcebedd98f2f720f8ed15290
Ilya2520 <91724425+Ilya2520@users.noreply.github.com>
2022-12-11 23:50:16 +0300

    Update scr.js

--- scr.js
+++ scr.js
@@ -20,9 +20,9 @@
 // })
 
 document.body.onload = function() {
-setTimeout(function() {
-var preloader = document.getElementById("preloader");
-if ( preloader.classList.contains('done')) {
-preloader.classList.add('preloader--hide');
+  setTimeout(function() {
+  var preloader = document.getElementById("preloader");
+  if ( preloader.classList.contains('done')) {
+  preloader.classList.add('preloader--hide');
 };
 }, 300);
