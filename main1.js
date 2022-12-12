975e9ea60f68981a54ab063ec967cb41710b3a9d
Ilya2520 <91724425+Ilya2520@users.noreply.github.com>
2022-12-12 11:22:50 +0300

    Update and rename main.js to main1.js

--- main1.js
+++ main1.js
@@ -1,11 +1,11 @@
 
 var endDate = new Date("Jan 15, 2023 12:00:00").getTime();
 var sc=window.screen.width;
-
+console.log(sc);
 var timer = setInterval(function() {
         let now=new Date();
     let t = endDate - now;
-    
+    console.log("something");
     if (t >= 0) {
     
         let days = Math.floor(t / (1000 * 60 * 60 * 24));
@@ -26,12 +26,11 @@ var timer = setInterval(function() {
         "<span class='label'>Секунд</span>";
         }
         else{
-            document.getElementById("timer-days").innerHTML = days +"(Д):"
-            "<span class='label1'>(Д):</span>";
+            document.getElementById("timer-days").innerHTML = days +"(Д):";
         
-            document.getElementById("timer-hours").innerHTML = ("0"+hours).slice(-2) +"(Ч):"
+            document.getElementById("timer-hours").innerHTML = ("0"+hours).slice(-2) +"(Ч):";
         
-            document.getElementById("timer-mins").innerHTML = ("0"+mins).slice(-2) + "(М)"
+            document.getElementById("timer-mins").innerHTML = ("0"+mins).slice(-2) + "(М)";
             
             document.getElementById("timer-days").style.fontSize="1.5rem";
             document.getElementById("timer-hours").style.fontSize="1.5rem";
